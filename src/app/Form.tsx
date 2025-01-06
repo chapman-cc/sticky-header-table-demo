d3a35f0684496c7fc9a46708c7ee9b831d5966db
Chapman Cheng <chapboy1989@gmail.com>
2025-01-06 11:27:53 +0000

    add native html form

--- src/app/Form.tsx
+++ src/app/Form.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Controller, useForm } from "react-hook-form";
-import React from "react";
+import React, { useId } from "react";
 import { Button, Checkbox } from "@dfds-frontend/compass-ui";
 
 interface FormSchema {
@@ -11,27 +11,55 @@ interface FormSchema {
 type a = React.JSX.IntrinsicElements["input"];
 
 export default function Form() {
-  const { control } = useForm<FormSchema>();
+  const { control, watch } = useForm<FormSchema>();
+  const idForNative = useId();
+
+  if (false)
+    // toggle here
+
+    return (
+      <div>
+        <h4>Value: {String(watch("isMarried"))}</h4>
+        <form>
+          <Controller
+            control={control}
+            name="isMarried"
+            render={({ field: { name, value, onChange, ref, onBlur } }) => (
+              <div className="flex gap-4">
+                <input
+                  type="checkbox"
+                  name={name}
+                  id={idForNative}
+                  checked={value}
+                  onChange={onChange}
+                  ref={ref}
+                />
+                <label htmlFor={idForNative}>Are you married?</label>
+              </div>
+            )}
+          />
+
+          <Button>Submit</Button>
+        </form>
+      </div>
+    );
+
   return (
-    <form>
-      <Controller
-        control={control}
-        name="isMarried"
-        render={({ field: { name, value, onChange, ref, onBlur } }) => (
-          <>
-            <Checkbox
-              name={name}
-              checked={value}
-              onChange={(e) => onChange(e.target.checked)}
-              ref={ref}
-            >
+    <div>
+      <h4>Value: {String(watch("isMarried"))}</h4>
+      <form>
+        <Controller
+          control={control}
+          name="isMarried"
+          render={({ field: { name, value, onChange, ref, onBlur } }) => (
+            <Checkbox name={name} checked={value} onChange={onChange} ref={ref}>
               Are you married?
             </Checkbox>
-          </>
-        )}
-      />
+          )}
+        />
 
-      <Button>Submit</Button>
-    </form>
+        <Button>Submit</Button>
+      </form>
+    </div>
   );
 }
